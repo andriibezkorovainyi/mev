3093770708ad6f7a7cd1b48cbcb975a1b58593c8
Andrii Bezkorovainyi <crm29865@gmail.com>
2024-05-01 11:57:33 +0300

    scipt-updateBalance

---
+++ packages/compound-v2-liquidator/scripts/updateAccountTokenBalance.ts
@@ -0,0 +1,29 @@
+import MasterModule from '../src/master/master.module.ts';
+
+async function updateAccountTokenBalance(): Promise<void> {
+  const masterModule = new MasterModule(undefined);
+  const accountService =
+    masterModule.accountModule.getService('accountService');
+  const storageService =
+    masterModule.storageModule.getService('storageService');
+
+  const pointerHeight = storageService.getPointerHeight();
+  const account = storageService.getAccount(
+    '0xFeECA8db8b5f4Efdb16BA43d3D06ad2F568a52E3',
+  );
+
+  console.log('Account:', account);
+  const cToken = '0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5';
+
+  const fetchedBalance = await accountService.fetchAccountTokenBalance(
+    account?.address!,
+    cToken,
+    pointerHeight,
+  );
+
+  account!.tokens[cToken] = fetchedBalance;
+
+  await storageService.cacheAccounts();
+}
+
+updateAccountTokenBalance();
